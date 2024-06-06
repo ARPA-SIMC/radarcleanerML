930795e89f40ea738571795b93be00c38a717640
Chiara <ccardinali@arpae.it>
2024-06-06 13:33:26 +0200

    script per la creazione dell'ambiente virtuale di lavoro

---
+++ setup_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+APP_NAME=${PWD##*/}
+echo "Rebuilding venv for ${APP_NAME}..."
+PY_EXE=python3
+rm -fr .venv/
+$PY_EXE -m venv .venv
+source .venv/bin/activate
+.venv/bin/$PY_EXE -m pip install --upgrade pip
+.venv/bin/$PY_EXE -m pip install -r requirements.txt
